6c55b6eb89ebcba923b3212db11276a6e87ee09e
uchexm <109084069+uchexm@users.noreply.github.com>
2022-11-16 18:38:05 +0200

    Add Modal

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const menu = document.querySelector('#mobile-menu');
 const menuItem = document.querySelector('.nav-menu');
+const popup = document.querySelector('#pop');
+const mcard = document.querySelector('.modal-card');
+const cross = document.querySelector('#ex');
 
 menu.addEventListener('click', () => {
   menu.classList.toggle('is-active');
@@ -10,3 +13,13 @@ menuItem.addEventListener('click', () => {
   menu.classList.toggle('is-active');
   menuItem.classList.toggle('active');
 });
+
+popup.addEventListener('click', () => {
+  popup.classList.toggle('is-active');
+  mcard.classList.toggle('active');
+});
+
+cross.addEventListener('click', () => {
+  cross.classList.toggle('is-active');
+  mcard.classList.toggle('active');
+})
